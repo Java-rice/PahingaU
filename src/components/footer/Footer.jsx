c7bcfd6d36d0419d2328df54d8d83d198d47c22e
John Patrick Lagatuz <patrick.zutagal@gmail.com>
2024-06-28 23:17:41 +0800

    added terms of service

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Logo from '../logo/Logo';
-import Socials from '../icons/Socials';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../logo/Logo";
+import Socials from "../icons/Socials";
 
 const Footer = () => {
   return (
@@ -10,31 +11,69 @@ const Footer = () => {
           <div className="w-full md:w-1/3 px-1 mb-8 gap-10 text-center md:text-left">
             <Logo />
             <p className="text-gray-700 mb-4 mt-4">
-              Nam posuere accumsan porta integer id orci sed ante tincidunt et amet sed libero.
+              Nam posuere accumsan porta integer id orci sed ante tincidunt et
+              amet sed libero.
             </p>
             <p className="text-gray-600">© AnoNameNatin 2024</p>
           </div>
           <div className="w-full md:w-1/5 px-1 mb-8 text-center md:text-left">
             <h4 className="text-lg font-bold mb-4 text-[#0077B5]">RENTING</h4>
             <ul className="space-y-2">
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">Browse Rentals</a></li>
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">How It Works</a></li>
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">Post My Property</a></li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  Browse Rentals
+                </a>
+              </li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  How It Works
+                </a>
+              </li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  Post My Property
+                </a>
+              </li>
             </ul>
           </div>
           <div className="w-full md:w-1/5 px-1 mb-8 text-center md:text-left">
-            <h4 className="text-lg font-bold mb-4 text-[#0077B5]">QUICK LINKS</h4>
+            <h4 className="text-lg font-bold mb-4 text-[#0077B5]">
+              QUICK LINKS
+            </h4>
             <ul className="space-y-2">
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">About Us</a></li>
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">Safety Guidelines</a></li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  Safety Guidelines
+                </a>
+              </li>
             </ul>
           </div>
           <div className="w-full md:w-1/5 px-1 mb-8 text-center md:text-left">
             <h4 className="text-lg font-bold mb-4 text-[#0077B5]">LEGAL</h4>
             <ul className="space-y-2">
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">Terms of Service</a></li>
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">Privacy Policy</a></li>
-              <li><a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">Cookie Policy</a></li>
+              <li>
+                <Link
+                  to="/TermsOfService"
+                  className="text-gray-700 hover:text-[#0077B5] cursor-pointer"
+                >
+                  Terms of Service
+                </Link>
+              </li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a className="text-gray-700 hover:text-[#0077B5] cursor-pointer">
+                  Cookie Policy
+                </a>
+              </li>
             </ul>
           </div>
           <div className="w-full px-2 mb-8 text-center justify-center md:text-left">
---
+++ src/pages/termsofservice/Termsofservice.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Termsofservice.css";
+
+const TermsOfService = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <button className="header-button">LEGAL</button>
+        <h1 className="header-title">Terms of Service</h1>
+      </div>
+      <div className="content"></div>
+    </div>
+  );
+};
+
+export default TermsOfService;
