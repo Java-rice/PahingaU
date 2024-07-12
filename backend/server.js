b836504b68d14073edc94e6dba26d8213a4e18a4
Java-rice <johnmark.p.peroche@gmail.com>
2024-07-12 08:26:39 +0800

    server

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,6 @@ const fs = require('fs');
 const app = express();
 const port = 3001;
 
-const upload = multer({ storage });
-
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
@@ -28,7 +26,7 @@ const storage = multer.diskStorage({
   }
 });
 
-
+const upload = multer({ storage: storage });
 
 // Connect to SQLite database
 const db = new sqlite3.Database('./database.sqlite', (err) => {
@@ -64,30 +62,38 @@ function createTables() {
     phone TEXT,
     password TEXT,
     profilePicture TEXT
-  )`);
-  db.run(`
-    CREATE TABLE IF NOT EXISTS housing (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      image TEXT,
-      title TEXT,
-      details TEXT,
-      price TEXT,
-      rating REAL,
-      reviews INTEGER,
-      latitude REAL,
-      longitude REAL
-    )
-  `);
+  )`, (err) => {
+    if (err) {
+      console.error('Error creating landlords table:', err.message);
+    }
+  });
 
-  // Create universities table and insert predefined coordinates
-  db.run(`
-    CREATE TABLE IF NOT EXISTS universities (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT UNIQUE,
-      latitude REAL,
-      longitude REAL
-    )
-  `);
+  db.run(`CREATE TABLE IF NOT EXISTS housing (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image TEXT,
+    title TEXT,
+    details TEXT,
+    price TEXT,
+    rating REAL,
+    reviews INTEGER,
+    latitude REAL,
+    longitude REAL
+  )`, (err) => {
+    if (err) {
+      console.error('Error creating housing table:', err.message);
+    }
+  });
+
+  db.run(`CREATE TABLE IF NOT EXISTS universities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE,
+    latitude REAL,
+    longitude REAL
+  )`, (err) => {
+    if (err) {
+      console.error('Error creating universities table:', err.message);
+    }
+  });
 
   db.run(`CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -97,58 +103,26 @@ function createTables() {
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (senderId) REFERENCES students(id),
     FOREIGN KEY (receiverId) REFERENCES landlords(id)
-  )`);
+  )`, (err) => {
+    if (err) {
+      console.error('Error creating messages table:', err.message);
+    }
+  });
 }
 
 app.post('/api/upload-profile-picture', upload.single('profilePicture'), (req, res) => {
   const userId = req.body.userId;
-  const table = req.body.isLandlord ? 'landlords' : 'students'; // Correct table name determination
-
-  const profilePicture = req.file.path;
+  const table = req.body.isLandlord === 'true' ? 'landlords' : 'students';
+  const profilePicture = `/uploads/${req.file.filename}`;
 
   db.run(`UPDATE ${table} SET profilePicture = ? WHERE id = ?`, [profilePicture, userId], function(err) {
-      if (err) {
-          return res.status(500).json({ error: err.message });
-      }
-      res.json({ message: 'Profile picture updated successfully', profilePicture });
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ message: 'Profile picture updated successfully', profilePicture });
   });
 });
 
-
-
-// Create universities table and insert predefined coordinates
-
-const universityCoordinates = {
-  "Adamson University": [120.986, 14.6042],
-  "Ateneo de Manila University": [121.0777, 14.6407],
-  "De La Salle University": [120.9932, 14.5648],
-  "De La Salle-College of Saint Benilde": [120.9951, 14.5636],
-  "National University, Philippines": [120.9946, 14.6043],
-  "Polytechnic University of the Philippines": [121.0108, 14.5979],
-  "University of Santo Tomas": [120.9896, 14.6093],
-  "University of the Philippines Diliman": [121.0657, 14.6537],
-  "University of the Philippines Manila": [120.9918, 14.5806],
-  "University of the Philippines System": [121.0657, 14.6537]
-};
-
-const insertUniversityQuery = `
-    INSERT INTO universities (name, latitude, longitude)
-    VALUES (?, ?, ?)
-  `;
-
-for (const [name, coords] of Object.entries(universityCoordinates)) {
-  db.run(insertUniversityQuery, [name, coords[1], coords[0]], function (err) {
-    if (err && err.message.includes('SQLITE_CONSTRAINT')) {
-      // The university already exists, no need to insert again
-      console.log(`University ${name} already exists`);
-      return;
-    } else if (err) {
-      console.error(`Error inserting university ${name}:`, err.message);
-    } else {
-      console.log(`University ${name} inserted`);
-    }
-  });
-}
 
 // Student registration endpoint
 app.post('/api/register/student', (req, res) => {
@@ -160,10 +134,10 @@ app.post('/api/register/student', (req, res) => {
       return res.status(500).json({ error: 'Error hashing password' });
     }
 
-    const query = `INSERT INTO students (fullName, email, university, socialStatus, phone, password, latitude, longitude, profilePicture) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const query = `INSERT INTO students (fullName, email, university, socialStatus, phone, password, latitude, longitude) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    db.run(query, [fullName, email, university, socialStatus, phone, hashedPassword, latitude, longitude, profilePicture], function (err) {
+    db.run(query, [fullName, email, university, socialStatus, phone, hashedPassword, latitude, longitude], function (err) {
       if (err) {
         return res.status(400).json({ error: err.message });
       }
@@ -182,10 +156,10 @@ app.post('/api/register/landlord', (req, res) => {
       return res.status(500).json({ error: 'Error hashing password' });
     }
 
-    const query = `INSERT INTO landlords (fullName, email, phone, password, profilePicture) 
-                     VALUES (?, ?, ?, ?, ?)`;
+    const query = `INSERT INTO landlords (fullName, email, phone, password) 
+                     VALUES (?, ?, ?, ?)`;
 
-    db.run(query, [fullName, email, phone, hashedPassword, profilePicture], function (err) {
+    db.run(query, [fullName, email, phone, hashedPassword], function (err) {
       if (err) {
         if (err.message.includes("UNIQUE constraint failed: landlords.email")) {
           return res.status(400).json({ error: 'Email already registered' });
@@ -219,7 +193,6 @@ app.get('/api/users/search', (req, res) => {
   });
 });
 
-
 // Search messages endpoint
 app.get('/api/messages/:chatId/:isLandlord', (req, res) => {
   const { chatId, isLandlord } = req.params;
@@ -243,8 +216,6 @@ app.get('/api/messages/:chatId/:isLandlord', (req, res) => {
   });
 });
 
-
-
 // Send message endpoint
 app.post('/api/messages/send', (req, res) => {
   const { senderId, receiverId, message, isLandlordSender } = req.body;
@@ -260,7 +231,6 @@ app.post('/api/messages/send', (req, res) => {
     res.json({ message: 'Message sent successfully', id: this.lastID });
   });
 });
-
 
 // Login endpoint
 app.post('/api/login', (req, res) => {
@@ -353,6 +323,38 @@ app.get('/api/universities', (req, res) => {
     res.json(rows); // Send universities data as JSON response
   });
 });
+
+const universityCoordinates = {
+  "Adamson University": [120.986, 14.6042],
+  "Ateneo de Manila University": [121.0777, 14.6407],
+  "De La Salle University": [120.9932, 14.5648],
+  "De La Salle-College of Saint Benilde": [120.9951, 14.5636],
+  "National University, Philippines": [120.9946, 14.6043],
+  "Polytechnic University of the Philippines": [121.0108, 14.5979],
+  "University of Santo Tomas": [120.9896, 14.6093],
+  "University of the Philippines Diliman": [121.0657, 14.6537],
+  "University of the Philippines Manila": [120.9918, 14.5806],
+  "University of the Philippines System": [121.0657, 14.6537]
+};
+
+const insertUniversityQuery = `
+    INSERT INTO universities (name, latitude, longitude)
+    VALUES (?, ?, ?)
+  `;
+
+for (const [name, coords] of Object.entries(universityCoordinates)) {
+  db.run(insertUniversityQuery, [name, coords[1], coords[0]], function (err) {
+    if (err && err.message.includes('SQLITE_CONSTRAINT')) {
+      // The university already exists, no need to insert again
+      console.log(`University ${name} already exists`);
+      return;
+    } else if (err) {
+      console.error(`Error inserting university ${name}:`, err.message);
+    } else {
+      console.log(`University ${name} inserted`);
+    }
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
